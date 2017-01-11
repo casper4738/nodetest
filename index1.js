7608ffab9c2df4bf7675c992a4e2281c67f9d6a6
Fandy Adam <casperadam91@gmail.com>
2017-01-11 07:12:18 +0700

    no message

---
+++ index1.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+
+var telegram_server = require('./application/telegram/telegram_server');
+var line_server = require('./application/line/app');
+var facebook_server = require('./application/facebook/app');
+var kaskus_server = require('./application/kaskus/kaskus_server');
+var test_server = require('./application/test/app');
+
+var imageDir = __dirname + "/images/";
+
+var PORT_HTTP = (process.env.PORT || 5000);
+var PORT_HTTPS = 8443;
+
+app.use('/telegram', telegram_server);
+app.use('/line', line_server);
+app.use('/kaskus', kaskus_server);
+app.use('/facebook', facebook_server);
+app.use('/test', test_server);
+
+//app.listen(8080);
+
+
+var credentials = {
+    //key: fs.readFileSync('cert/chatbot_uatmb_com-key.pem'),
+    // cert: fs.readFileSync('cert/chatbot_uatmb_com-cert.pem')
+};
+
+var httpServer = http.createServer(app);
+//var httpsServer = https.createServer(credentials, app);
+//var httpsServer = https.createServer(app);
+
+
+app.get("/images/:id", function(request, response) {
+    var id = request.params.id;
+    var path = imageDir + id;
+    console.log("fetching image: ", path);
+    response.sendFile(path);
+});
+
+
+httpServer.listen(PORT_HTTP, function() {
+    console.log("I'm working http:)");
+});
+
+//httpsServer.listen(PORT_HTTPS, function() {
+//    console.log("I'm working https:)");
+//});
